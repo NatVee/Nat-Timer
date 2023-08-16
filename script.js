13f135c26400453386c1f8074ae1752d54dc7040
NatVee <102380124+NatVee@users.noreply.github.com>
2023-08-16 14:30:45 +0300

    fixing bug with display 2

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function updateCountdown(){
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
     seconds = seconds < 10 ? "0" + seconds: seconds;
-    //countDownEl.innerHTML = `${minutes}:${seconds}`;
-    countDownEl.innerHTML = 'lol';
+    countDownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
 }

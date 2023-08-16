d42e6e0a428308e477478daecfa698e5132de199
NatVee <102380124+NatVee@users.noreply.github.com>
2023-08-16 18:08:59 +0300

    fixing bugs with time

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ buttonEl.addEventListener('click', function(){
 
 function updateCountdown(){
     let days = Math.floor(time / (3600 * 24));
-    time %= (3600 * 24);
-    let hours = Math.floor(time / (3600));
-    time %= (3600)   
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+    let t = time % (3600 * 24)
+    let hours = Math.floor(t / (3600));
+    t %= (3600)   
+    let minutes = Math.floor(t / 60);
+    let seconds = t % 60;
     hours = hours < 10 ? "0" + hours: hours;
     minutes = minutes < 10 ? "0" + minutes: minutes;
     seconds = seconds < 10 ? "0" + seconds: seconds;
